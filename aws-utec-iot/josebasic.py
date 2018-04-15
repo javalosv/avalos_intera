d6748afe92b004c4373ad42058e512544f3d19a2
Jose Avalos <jose.avalos@utec.edu.pe>
2018-04-15 00:08:38 -0500

    add publisher mqtt

---
+++ aws-utec-iot/josebasic.py
@@ -0,0 +1,78 @@
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+import time
+import argparse
+import json
+
+import rospy
+from std_msgs.msg import String
+from avalos_intera.msg import robot_q
+
+
+AllowedActions = ['both', 'publish', 'subscribe']
+
+
+class SendIOT:
+    def __init__(self):
+        host = "a2ujlqua2mmq82.iot.us-west-2.amazonaws.com" #args.host
+        rootCAPath = "root-CA.crt"
+        certificatePath = "prueba_python.cert.pem"
+        privateKeyPath = "prueba_python.private.key"
+        useWebsocket = False
+        clientId = "JoseAvalos_PubSub"
+        topic = "data/q"
+        mode="publish" #"-m", "--mode", action="store", dest="mode", default="both",
+
+        # Init AWSIoTMQTTClient
+        myAWSIoTMQTTClient = None
+        myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+        myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+        myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+        # AWSIoTMQTTClient connection configuration
+        myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+        myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+        myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+        myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+        myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+        myAWSIoTMQTTClient.connect()
+
+
+        self.pub_msg=[0,0,0,0,0,0,0,0]
+        rospy.Subscriber("avalos/iot/values_q", robot_q, self.sender)
+
+        if mode == 'both' or mode == 'subscribe':
+            myAWSIoTMQTTClient.subscribe(topic, 1, self.customCallback)
+            time.sleep(0.05)
+        while not rospy.is_shutdown():
+            if mode == 'both' or mode == 'publish':
+                message = {}
+                message['head'] = self.pub_msg[0]
+                message['right_j0'] = self.pub_msg[1]
+                message['right_j1'] = self.pub_msg[2]
+                message['right_j2'] = self.pub_msg[3]
+                message['right_j3'] = self.pub_msg[4]
+                message['right_j4'] = self.pub_msg[5]
+                message['right_j5'] = self.pub_msg[6]
+                message['right_j6'] = self.pub_msg[7]
+                messageJson = json.dumps(message)
+                myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+
+    # Custom MQTT message callback
+    def customCallback(client, userdata, message):
+    #    print(message.topic)
+        print("recived msg\n")
+
+    def sender(self,d): 
+        self.pub_msg=d.angle
+
+if __name__ == '__main__':
+       
+    try:
+        rospy.init_node('node_mqtt', anonymous=True)
+        send=SendIOT()
+        print("begin send... ")
+    except rospy.ROSInterruptException:
+        pass
+    
--- scripts/test/new_topic.py
+++ scripts/test/new_topic.py
@@ -3,24 +3,29 @@
 import rospy
 from std_msgs.msg import String
 from avalos_intera.msg import robot_q
+from sensor_msgs.msg import JointState
 
 class Bridge:
     def __init__(self):
-        rospy.Subscriber("/robot/joint_states", JointState, callback)
-
-    def talker():
-        """
-        pub = rospy.Publisher('chatter', String, queue_size=10)
+        self.pub_msg=robot_q()
+        rospy.Subscriber("/robot/joint_states", JointState, self.talker)
+        pub = rospy.Publisher('avalos/iot/values_q', robot_q, queue_size=10)
+        print("Init bridge")
         rate = rospy.Rate(2) # 10hz
         while not rospy.is_shutdown():
-            hello_str = "hello world %s" % rospy.get_time()
-            rospy.loginfo(hello_str)
-            pub.publish(hello_str)
+            pub.publish(self.pub_msg)
             rate.sleep()
-        """
+    
+    def talker(self,d):            
+        self.pub_msg.name=[d.name[0],d.name[1],d.name[2],d.name[3],d.name[4],d.name[5],d.name[6],d.name[7]]
+        self.pub_msg.angle=[d.position[0],d.position[1],d.position[2],d.position[3],d.position[4],d.position[5],d.position[6],d.position[7]]
+
 if __name__ == '__main__':
+    
     try:
-        rospy.init_node('talker', anonymous=True)
+        rospy.init_node('node_q', anonymous=True)
+        bridge=Bridge()
+        print("Enabling robot... ")
     except rospy.ROSInterruptException:
         pass
-    bridge=Bridge()
+    
