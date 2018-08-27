1611a3eec92045261417efb8dd2baad6c6c1fd93
JAVA <jose.avalos@utec.edu.pe>
2018-08-27 11:34:30 -0500

    Add spline 7th s6

--- scripts/test/s6/intera_avalos.py
+++ scripts/test/s6/intera_avalos.py
@@ -116,7 +116,64 @@ def generate_path_cub(_points,_time,_f,p=True):
 		print "Knots en posicion generados.",ext
 	return q,v,a,y
 
-def path_simple_cub_v0(_point,_time,_f,jerk_value=False):
+def path_simple_cub_get_jerk(_point,_time,_f):
+	x=_time
+	a=_point
+	f=_f
+	FPO=0.0
+	FPN=0.0
+
+	n=len(a)-1;
+	l=np.zeros(n+1, dtype=np.float_)
+	u=np.zeros(n, dtype=np.float_)
+	z=np.zeros(n+1, dtype=np.float_)
+	h=np.zeros(n, dtype=np.float_)
+	alfa=np.zeros(n+1, dtype=np.float_)
+	c=np.zeros(n+1, dtype=np.float_)
+	b=np.zeros(n, dtype=np.float_)
+	d=np.zeros(n, dtype=np.float_)
+
+	for i in range(n):
+		h[i]=x[i+1]-x[i]
+	sA = np.zeros(shape=(n+1,n+1), dtype=np.float_)
+	for i in range(n-1) :
+		for j in range(n-1) :
+			if i is j:
+				sA[i+1][i+1]=2*(h[i]+h[i+1])
+				sA[i+1][i]=h[i]
+				sA[i][i+1]=h[i]
+	sA[0][0]=2*h[0]
+	sA[-1][-1]=2*h[-1]
+	sA[-1][-2]=h[-1]
+	sA[-2][-1]=h[-1]
+
+	sb = np.zeros(shape=(n+1,1), dtype=np.float_)
+
+	for i in range(1,n) :
+		sb[i]=(3.0*(a[i+1]-a[i])/h[i]) - (3.0*(a[i]-a[i-1])/h[i-1])
+
+	sb[0]=((3.0*(a[1]-a[0]))/h[0])-3.0*FPO
+	sb[-1]=3.0*FPN-(3.0*(a[n]-a[n-1])/h[n-1])
+
+	_b=np.arange(n, dtype=np.float_)
+	_c=np.linalg.solve(sA, sb)
+	_d=np.arange(n, dtype=np.float_)
+
+	for j in reversed(range(n)):
+		_b[j]=((a[j+1]-a[j])/h[j] )-(h[j]*(_c[j+1]+2*_c[j])/3.0)
+		_d[j]=(_c[j+1]-_c[j])/(3.0*h[j])
+
+	t_out=np.linspace(x[0], x[-1], int((x[-1]-x[0])*f)+1)
+	tl=len(t_out)
+	y =np.zeros(tl, dtype=np.float_)
+	for i in range(n):
+		for j in range(tl):
+			if(t_out[j]>=x[i] and t_out[j]<x[i+1]):
+				y[j]=6*_d[i]
+	y[-1]=y[-2]
+	return y
+
+def path_simple_cub_v0(_point,_time,_f):
 	x=_time
 	a=_point
 	f=_f
@@ -170,26 +227,58 @@ def path_simple_cub_v0(_point,_time,_f,jerk_value=False):
 	v =np.zeros(tl, dtype=np.float_)
 	ac =np.zeros(tl, dtype=np.float_)
 	y =np.zeros(tl, dtype=np.float_)
-	if(jerk_value):
-		for i in range(n):
-			for j in range(tl):
-				if(t_out[j]>=x[i] and t_out[j]<x[i+1]):
-					y[j]=6*_d[i]
-		y[-1]=y[-2]
-		return y
-	else:
-		for i in range(n):
-			for j in range(tl):
-				if(t_out[j]>=x[i] and t_out[j]<x[i+1]):
-					p[j]=( a[i]+_b[i]*(t_out[j]-x[i])+_c[i]*(t_out[j]-x[i])**2+_d[i]*(t_out[j]-x[i])**3)
-					v[j]=_b[i]+2*_c[i]*(t_out[j]-x[i])+3*_d[i]*(t_out[j]-x[i])**2
-					ac[j]=2*_c[i]+6*_d[i]*(t_out[j]-x[i])
-					y[j]=6*_d[i]
-		p[-1]=a[-1]
-		v[-1]=0
-		ac[-1]=ac[-2] # Aceleracion misma letra que a como polinomio
-		y[-1]=y[-2]
-		return p,v,ac,y,tl
+
+	for i in range(n):
+		for j in range(tl):
+			if(t_out[j]>=x[i] and t_out[j]<x[i+1]):
+				p[j]=( a[i]+_b[i]*(t_out[j]-x[i])+_c[i]*(t_out[j]-x[i])**2+_d[i]*(t_out[j]-x[i])**3)
+				v[j]=_b[i]+2*_c[i]*(t_out[j]-x[i])+3*_d[i]*(t_out[j]-x[i])**2
+				ac[j]=2*_c[i]+6*_d[i]*(t_out[j]-x[i])
+				y[j]=6*_d[i]
+	k1=int((h[0])*f)
+	k2=int((h[-1])*f+1)
+
+	s_v=v[k1]
+	s_ac=ac[k1]
+	s_y=y[k1]
+	h_0=h[0]
+	h_e=h[-1]
+
+	# Spline 7 grade begin
+	a00 =(10*s_v)/h_0**6 - (2*s_ac)/h_0**5 + s_y/(6*h_0**4) + (20*(a[0]-a[1]))/h_0**7
+	a01 =(13*s_ac)/(2*h_0**4) - (34*s_v)/h_0**5 - s_y/(2*h_0**3) - (70*(a[0]-a[1]))/h_0**6
+	a02 =(39*s_v)/h_0**4 - (7*s_ac)/h_0**3 + s_y/(2*h_0**2) + (84*(a[0]-a[1]))/h_0**5
+	a03 =(5*s_ac)/(2*h_0**2) - (15*s_v)/h_0**3 - s_y/(6*h_0) - (35*(a[0]-a[1]))/h_0**4
+	for j in range(k1):
+	    p[j]=a00*(j*0.01)**7+a01*(j*0.01)**6+a02*(j*0.01)**5+a03*(j*0.01)**4+a[0]
+	    v[j]=7*a00*(j*0.01)**6+6*a01*(j*0.01)**5+5*a02*(j*0.01)**4+4*a03*(j*0.01)**3
+	    ac[j]=42*a00*(j*0.01)**5+30*a01*(j*0.01)**4+20*a02*(j*0.01)**3+12*a03*(j*0.01)**2
+	    y[j]=210*a00*(j*0.01)**4+120*a01*(j*0.01)**3+60*a02*(j*0.01)**2+24*a03*(j*0.01)**1
+
+	#  Spline 7 grade end
+	a7=a[-2]
+	a6=v[-k2]
+	a5= ac[-k2]/2
+	a4= -y[-k2]/6
+	h_e=h[-1]
+	p_e=a[-1]
+
+	ak0 = (2*(2*a5 + 6*a4*h_e))/h_e**5 - a4/h_e**4 - (10*(3*a4*h_e**2 + 2*a5*h_e + a6))/h_e**6 + (20*(a4*h_e**3 + a5*h_e**2 + a6*h_e + a7 - p_e))/h_e**7
+	ak1 = (3*a4)/h_e**3 - (13*(2*a5 + 6*a4*h_e))/(2*h_e**4) + (34*(3*a4*h_e**2 + 2*a5*h_e + a6))/h_e**5 - (70*(a4*h_e**3 + a5*h_e**2 + a6*h_e + a7 - p_e))/h_e**6
+	ak2 = (7*(2*a5 + 6*a4*h_e))/h_e**3 - (3*a4)/h_e**2 - (39*(3*a4*h_e**2 + 2*a5*h_e + a6))/h_e**4 + (84*(a4*h_e**3 + a5*h_e**2 + a6*h_e + a7 - p_e))/h_e**5
+	ak3 = a4/h_e - (5*(2*a5 + 6*a4*h_e))/(2*h_e**2) + (15*(3*a4*h_e**2 + 2*a5*h_e + a6))/h_e**3 - (35*(a4*h_e**3 + a5*h_e**2 + a6*h_e + a7 - p_e))/h_e**4
+	ak4 =a4
+	ak5 =a5
+	ak6 =a6
+	ak7 =a7
+
+	for j in range(k2):
+	    p[-k2+j]=ak0*(j*0.01)**7+ak1*(j*0.01)**6+ak2*(j*0.01)**5+ak3*(j*0.01)**4+ak4*(j*0.01)**3+ak5*(j*0.01)**2+ak6*(j*0.01)+ak7
+	    v[-k2+j]=7*ak0*(j*0.01)**6+6*ak1*(j*0.01)**5+5*ak2*(j*0.01)**4+4*ak3*(j*0.01)**3+3*ak4*(j*0.01)**2+2*ak5*(j*0.01)+ak6
+	    ac[-k2+j]=42*ak0*(j*0.01)**5+30*ak1*(j*0.01)**4+20*ak2*(j*0.01)**3+12*ak3*(j*0.01)**2+6*ak4*(j*0.01)+2*ak5
+	    y[-k2+j]=210*ak0*(j*0.01)**4+120*ak1*(j*0.01)**3+60*ak2*(j*0.01)**2+24*ak3*(j*0.01)**1+6*ak4
+
+	return p,v,ac,y,tl
 
 	# Se determina el minimo tiempo para ejecutar el movimiento
 
@@ -224,13 +313,13 @@ class Opt_1_avalos():
 		ecu=self.alfa*self.value_t+(1-self.alfa)*self.value_jk
 		return ecu
 	def value_sum_jerk(self,_points,_time,_f):
-		jk0=path_simple_cub_v0(_points[0],_time,_f,jerk_value=True)
-		jk1=path_simple_cub_v0(_points[1],_time,_f,jerk_value=True)
-		jk2=path_simple_cub_v0(_points[2],_time,_f,jerk_value=True)
-		jk3=path_simple_cub_v0(_points[3],_time,_f,jerk_value=True)
-		jk4=path_simple_cub_v0(_points[4],_time,_f,jerk_value=True)
-		jk5=path_simple_cub_v0(_points[5],_time,_f,jerk_value=True)
-		jk6=path_simple_cub_v0(_points[6],_time,_f,jerk_value=True)
+		jk0=path_simple_cub_get_jerk(_points[0],_time,_f)
+		jk1=path_simple_cub_get_jerk(_points[1],_time,_f)
+		jk2=path_simple_cub_get_jerk(_points[2],_time,_f)
+		jk3=path_simple_cub_get_jerk(_points[3],_time,_f)
+		jk4=path_simple_cub_get_jerk(_points[4],_time,_f)
+		jk5=path_simple_cub_get_jerk(_points[5],_time,_f)
+		jk6=path_simple_cub_get_jerk(_points[6],_time,_f)
 		ext= len(jk0)
 		a_jk0=get_area(jk0,_f)
 		a_jk1=get_area(jk1,_f)
@@ -269,7 +358,7 @@ class Opt_2_avalos():
 		print "Working in solution alfa=",str(_alfa)
 
 		#print bnds
-		self.res = minimize(self.costo, x0,method='SLSQP', bounds=bnds, tol=0.01,options={ 'disp': False})
+		self.res = minimize(self.costo, x0,method='SLSQP', bounds=bnds, tol=0.1,options={ 'disp': False})
 	def costo(self,k):
 		t=k*self.delta_t
 		for i in range(self.l):
@@ -281,13 +370,13 @@ class Opt_2_avalos():
 		ecu=self.alfa*self.value_t+(1-self.alfa)*self.value_jk
 		return ecu
 	def value_sum_jerk(self,_points,_time,_f):
-		jk0=path_simple_cub_v0(_points[0],_time,_f,jerk_value=True)
-		jk1=path_simple_cub_v0(_points[1],_time,_f,jerk_value=True)
-		jk2=path_simple_cub_v0(_points[2],_time,_f,jerk_value=True)
-		jk3=path_simple_cub_v0(_points[3],_time,_f,jerk_value=True)
-		jk4=path_simple_cub_v0(_points[4],_time,_f,jerk_value=True)
-		jk5=path_simple_cub_v0(_points[5],_time,_f,jerk_value=True)
-		jk6=path_simple_cub_v0(_points[6],_time,_f,jerk_value=True)
+		jk0=path_simple_cub_get_jerk(_points[0],_time,_f)
+		jk1=path_simple_cub_get_jerk(_points[1],_time,_f)
+		jk2=path_simple_cub_get_jerk(_points[2],_time,_f)
+		jk3=path_simple_cub_get_jerk(_points[3],_time,_f)
+		jk4=path_simple_cub_get_jerk(_points[4],_time,_f)
+		jk5=path_simple_cub_get_jerk(_points[5],_time,_f)
+		jk6=path_simple_cub_get_jerk(_points[6],_time,_f)
 		ext= len(jk0)
 		a_jk0=get_area(jk0,_f)
 		a_jk1=get_area(jk1,_f)
--- scripts/test/s6/simulacion_caso1.py
+++ scripts/test/s6/simulacion_caso1.py
@@ -16,71 +16,66 @@ import matplotlib.pyplot as plt
 # Python 3.5
 def main():
 
-    try:
-
-        #frecuency for Sawyer robot
-        f=100
-        #Initial position
-        ik_pose_1=np.array([0.45,0.70,-0.2,0.70,0.0,0.70,0.0])
-        ik1=np.array([ 0.69514791 , 0.64707899  ,1.92295654 , 0.01856896, -0.96413306, -0.92232169 , 4.16828131 ])
-        ik_pose_2=np.array([0.80,0.30,0.3,0.70,0.0,0.70,0.0])
-        ik2=np.array([5.97103602e-01 , -9.58042104e-02 ,  1.70891311e+00,  -9.48490850e-01,   -1.16882802e-03,   3.46304028e-01,   3.15488999e+00])
-        ik_pose_3=np.array([0.50,0.00,0.65,0.70,0.0,0.70,0.0])
-        ik3=np.array([ 0.63314606 ,-0.74365565,  1.14243681, -1.8618414,  -0.84360096,  1.44537886 , 3.41899404])
-        ik_pose_4=np.array([0.80,-0.30,0.3,0.70,0.0,0.70,0.0])
-        ik4=np.array([-0.31256034, -0.32949631,  2.12830197, -1.18751871, -0.45027567 , 1.32627167,  2.95775708])
-        ik_pose_5=np.array([0.45,-0.70,-0.20,0.70,0.0,0.70,0.0])
-        ik5=np.array([-1.55624859,  0.70439442,  2.52311113 ,-0.08708148 ,-0.94109983,  1.61554785  ,2.54289819])
-
-        q=np.array([[ik1[0],ik2[0],ik3[0],ik4[0],ik5[0]], \
-                [ik1[1],ik2[1],ik3[1],ik4[1],ik5[1]], \
-                [ik1[2],ik2[2],ik3[2],ik4[2],ik5[2]], \
-                [ik1[3],ik2[3],ik3[3],ik4[3],ik5[3]], \
-                [ik1[4],ik2[4],ik3[4],ik4[4],ik5[4]], \
-                [ik1[5],ik2[5],ik3[5],ik4[5],ik5[5]], \
-                [ik1[6],ik2[6],ik3[6],ik4[6],ik5[6]] \
-                ])
-
-        t_min, t_min_tiempo=min_time(q)
-        print t_min_tiempo
-        tasa=1/0.2
-        knots_sec=np.round(t_min*tasa,0)
-        t_k2=np.arange(knots_sec[-1])
-        k_j0 = sp.interpolate.interp1d(knots_sec, [ik1[0],ik2[0],ik3[0],ik4[0],ik5[0]], kind='linear')(t_k2)
-        k_j1 = sp.interpolate.interp1d(knots_sec, [ik1[1],ik2[1],ik3[1],ik4[1],ik5[1]], kind='linear')(t_k2)
-        k_j2 = sp.interpolate.interp1d(knots_sec, [ik1[2],ik2[2],ik3[2],ik4[2],ik5[2]], kind='linear')(t_k2)
-        k_j3 = sp.interpolate.interp1d(knots_sec, [ik1[3],ik2[3],ik3[3],ik4[3],ik5[3]], kind='linear')(t_k2)
-        k_j4 = sp.interpolate.interp1d(knots_sec, [ik1[4],ik2[4],ik3[4],ik4[4],ik5[4]], kind='linear')(t_k2)
-        k_j5 = sp.interpolate.interp1d(knots_sec, [ik1[5],ik2[5],ik3[5],ik4[5],ik5[5]], kind='linear')(t_k2)
-        k_j6 = sp.interpolate.interp1d(knots_sec, [ik1[6],ik2[6],ik3[6],ik4[6],ik5[6]], kind='linear')(t_k2)
-        q=np.array([k_j0,k_j1,k_j2,k_j3,k_j4,k_j5,k_j6])
-
-        alfa=[0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.85,0.90,0.95]
-        l_alfa=len(alfa)
-        v_t=np.ones(l_alfa)
-        v_jk=np.ones(l_alfa)
-        for i in xrange(l_alfa):
-            start = time.time()
-            opt=Opt_2_avalos(q,f,alfa[i])
-            v_time=opt.value()
-            end = time.time()
-            print 'Process Time:', end-start
-            #print "Vector Time",v_time
-            v_t[i]=opt.value_time()
-            v_jk[i]=opt.value_jerk()
-            print 'Costo Tiempo:',v_t[i]
-            print 'Costo Jerk:',v_jk[i]
-        plt.plot(v_t,v_jk,'r*',v_t,v_jk,)
-        plt.xlabel("Variable_tiempo")
-        plt.ylabel("Variable_jerk")
-        plt.show()
-        print "Programa terminado."
 
 
-    except:
-        print "Error"
-        sys.exit(1)
-    #    rospy.logerr('Keyboard interrupt detected from the user.')
+    #frecuency for Sawyer robot
+    f=100
+    #Initial position
+    ik_pose_1=np.array([0.45,0.70,-0.2,0.70,0.0,0.70,0.0])
+    ik1=np.array([ 0.69514791 , 0.64707899  ,1.92295654 , 0.01856896, -0.96413306, -0.92232169 , 4.16828131 ])
+    ik_pose_2=np.array([0.80,0.30,0.3,0.70,0.0,0.70,0.0])
+    ik2=np.array([5.97103602e-01 , -9.58042104e-02 ,  1.70891311e+00,  -9.48490850e-01,   -1.16882802e-03,   3.46304028e-01,   3.15488999e+00])
+    ik_pose_3=np.array([0.50,0.00,0.65,0.70,0.0,0.70,0.0])
+    ik3=np.array([ 0.63314606 ,-0.74365565,  1.14243681, -1.8618414,  -0.84360096,  1.44537886 , 3.41899404])
+    ik_pose_4=np.array([0.80,-0.30,0.3,0.70,0.0,0.70,0.0])
+    ik4=np.array([-0.31256034, -0.32949631,  2.12830197, -1.18751871, -0.45027567 , 1.32627167,  2.95775708])
+    ik_pose_5=np.array([0.45,-0.70,-0.20,0.70,0.0,0.70,0.0])
+    ik5=np.array([-1.55624859,  0.70439442,  2.52311113 ,-0.08708148 ,-0.94109983,  1.61554785  ,2.54289819])
+
+    q=np.array([[ik1[0],ik2[0],ik3[0],ik4[0],ik5[0]], \
+            [ik1[1],ik2[1],ik3[1],ik4[1],ik5[1]], \
+            [ik1[2],ik2[2],ik3[2],ik4[2],ik5[2]], \
+            [ik1[3],ik2[3],ik3[3],ik4[3],ik5[3]], \
+            [ik1[4],ik2[4],ik3[4],ik4[4],ik5[4]], \
+            [ik1[5],ik2[5],ik3[5],ik4[5],ik5[5]], \
+            [ik1[6],ik2[6],ik3[6],ik4[6],ik5[6]] \
+            ])
+
+    t_min, t_min_tiempo=min_time(q)
+    print t_min_tiempo
+    tasa=1/0.2
+    knots_sec=np.round(t_min*tasa,0)
+    t_k2=np.arange(knots_sec[-1])
+    k_j0 = sp.interpolate.interp1d(knots_sec, [ik1[0],ik2[0],ik3[0],ik4[0],ik5[0]], kind='linear')(t_k2)
+    k_j1 = sp.interpolate.interp1d(knots_sec, [ik1[1],ik2[1],ik3[1],ik4[1],ik5[1]], kind='linear')(t_k2)
+    k_j2 = sp.interpolate.interp1d(knots_sec, [ik1[2],ik2[2],ik3[2],ik4[2],ik5[2]], kind='linear')(t_k2)
+    k_j3 = sp.interpolate.interp1d(knots_sec, [ik1[3],ik2[3],ik3[3],ik4[3],ik5[3]], kind='linear')(t_k2)
+    k_j4 = sp.interpolate.interp1d(knots_sec, [ik1[4],ik2[4],ik3[4],ik4[4],ik5[4]], kind='linear')(t_k2)
+    k_j5 = sp.interpolate.interp1d(knots_sec, [ik1[5],ik2[5],ik3[5],ik4[5],ik5[5]], kind='linear')(t_k2)
+    k_j6 = sp.interpolate.interp1d(knots_sec, [ik1[6],ik2[6],ik3[6],ik4[6],ik5[6]], kind='linear')(t_k2)
+    q=np.array([k_j0,k_j1,k_j2,k_j3,k_j4,k_j5,k_j6])
+
+    alfa=[0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50,0.55,0.60,0.65,0.70,0.75,0.80,0.85,0.90,0.95]
+    l_alfa=len(alfa)
+    v_t=np.ones(l_alfa)
+    v_jk=np.ones(l_alfa)
+    for i in xrange(l_alfa):
+        start = time.time()
+        opt=Opt_2_avalos(q,f,alfa[i])
+        v_time=opt.value()
+        end = time.time()
+        print 'Process Time:', end-start
+        #print "Vector Time",v_time
+        v_t[i]=opt.value_time()
+        v_jk[i]=opt.value_jerk()
+        print 'Costo Tiempo:',v_t[i]
+        print 'Costo Jerk:',v_jk[i]
+    plt.plot(v_t,v_jk,'r*',v_t,v_jk,)
+    plt.xlabel("Variable_tiempo")
+    plt.ylabel("Variable_jerk")
+    plt.show()
+    print "Programa terminado."
+
 
 if __name__ == '__main__':
     main()
